419ef601e0fc71be14df35c1b546f0c67e19c76c
Johan Larsson <johanlarsson@outlook.com>
2021-10-18 14:44:18 +0200

    Comment out deprecated option

--- neovim/.config/nvim/lua/config/vimtex.lua
+++ neovim/.config/nvim/lua/config/vimtex.lua
@@ -9,7 +9,7 @@ g.vimtex_view_method = "zathura"
 g.vimtex_quickfix_open_on_warning = 0
 g.vimtex_syntax_conceal = { foo = true }
 g.tex_conecal = ""
-g.vimtex_syntax_conceal_default = 0
+-- g.vimtex_syntax_conceal_default = 0
 
 -- " let g:vimtex_view_general_viewer = 'okular'
 -- " let g:vimtex_view_general_options = '--unique file:@pdf\#src:@line@tex'
