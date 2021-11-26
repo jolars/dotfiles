d5d9ccf34c8efc03aedc9cf5025fb4916e516709
Johan Larsson <johanlarsson@outlook.com>
2021-11-26 07:59:16 +0100

    Remove unused autocommands

--- neovim/.config/nvim/lua/autocommands.lua
+++ neovim/.config/nvim/lua/autocommands.lua
@@ -13,25 +13,3 @@ cmd([[
   au TermLeave * setlocal scrolloff=3
 ]])
 
--- cmd([[
---   augroup Packer
---     autocmd!
---     autocmd!
---   bufs BufWritePost
---     */plugin/init.lua
---       lua refreshPlugins()
--- ]])
-  
--- vim.cmd [[ autocmd CursorHold,CursorHoldI * lua LightBulbFunction() ]]
--- vim.cmd 'au CursorHold,CursorHoldI <buffer> lua require"nvim-lightbulb".update_lightbulb {sign = {enabled = false}, virtual_text = {enabled = true, text = ""}, float = {enabled = false, text = "", win_opts = {winblend = 100, anchor = "NE"}}}'
-
--- vim.cmd(
---   [[
---   augroup DetectIndent
---      autocmd!
---      autocmd BufReadPost *  DetectIndent
---   augroup END
--- ]],
---   false
--- )
-
